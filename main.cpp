8da139503bddb876e5c2ca295a042614c258d12e
jsharris1999 <jaredscott1999@gmail.com>
2021-08-07 09:57:12 -0500

    Add files via upload
    Added functionality to detect if the user entered their file extension or not and adds it if it is not there. Also checks for supported file extensions when doing so.

--- main.cpp
+++ main.cpp
@@ -8,16 +8,46 @@
 #include <algorithm>
 #include "Villain.h"
 
+//Auxilliary Function to Check if a String Contains a Specific Substring
+bool Contains(string ToCheck, string Substring)
+{
+	if (ToCheck.find(Substring) != string::npos) { return true; }
+	return false;
+}
+
+//Auxilliary Function to add Supported Filetype Extensions if the User does not Provide one
+string AddExtension(string ToAdd)
+{
+	string Filetype = "";
+	cout << "Please enter the type of the file containing the deck you would like to use." << endl;
+	cout << "Currently supported filetypes include '.txt' files." << endl;
+	cin >> Filetype;
+	while(Filetype.compare(".txt") != 0)
+	{
+		Filetype = "";
+		cout << "The type of file you have entered is not supported, please enter a different filetype or enter q to close the program." << endl;
+		cout << "Currently supported filetypes include '.txt' files." << endl;
+		cin >> Filetype;
+		if (Filetype == "Q" || Filetype == "q") { return "q"; }
+	}
+	if(Filetype.compare(".txt") == 0) { ToAdd = ToAdd.append(".txt"); }
+	return ToAdd;
+}
+
 int main()
 {
 	//Initialization Variables
 	string Filename = "";
     Villain newVillain;
     
-    //Get Filename and add .txt to the end
+    //Get Filename, Filetype, and add a file extension to the end if necessary
     cout << "Please enter the name of the file containing the deck you would like to use." << endl;
     cin >> Filename;
-	Filename = Filename.append(".txt");
+    if(Contains(Filename, ".txt") == false)
+    {
+		Filename = AddExtension(Filename);
+		if (Filename == "Q" || Filename == "q") { return 0; }
+	}
 	
 	// Check if the file can be opened
     while (newVillain.readFile(Filename) == false)
@@ -25,8 +55,12 @@ int main()
 		Filename = "";
 		cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
 		cin >> Filename;
-		Filename = Filename.append(".txt");
 		if (Filename == "Q" || Filename == "q") { return 0; }
+		if(Contains(Filename, ".txt") == false)
+		{ 
+			Filename = AddExtension(Filename);
+			if (Filename == "Q" || Filename == "q") { return 0; }
+		}
 	}
 	
 	//Shuffle the deck and play the game
