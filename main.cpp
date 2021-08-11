f9514195918f0d6e1681fcda8013d312ee10c8a1
jsharris1999 <jaredscott1999@gmail.com>
2021-08-11 13:48:58 -0500

    Add files via upload
    Cleaned up the request for the file with the villain deck and fixed a bug with the AddExtension method

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ string AddExtension(string ToAdd)
 {
 	string Filetype = "";
 	cout << "Please enter the type of the file containing the deck you would like to use." << endl;
-	cout << "Currently supported filetypes include '.txt' files." << endl;
+	cout << "Currently supported filetypes include '.txt', '.csv', and '.tsv' files." << endl;
 	cin >> Filetype;
-	while(Filetype.compare(".txt") != 0 || Filetype.compare(".csv") != 0 || Filetype.compare(".tsv") != 0)
+	while(Filetype.compare(".txt") != 0 && Filetype.compare(".csv") != 0 && Filetype.compare(".tsv") != 0)
 	{
 		Filetype = "";
 		cout << "The type of file you have entered is not supported, please enter a different filetype or enter q to close the program." << endl;
@@ -40,64 +40,63 @@ int main()
 {
 	//Initialization Variables
 	string Filename = "";
+	bool SuccessfulRead = false;
     Villain newVillain;
     
     //Get Filename, Filetype, and add a file extension to the end if necessary
-    cout << "Please enter the name of the file containing the deck you would like to use." << endl;
-    cin >> Filename;
-    if(Contains(Filename, ".txt") == false && Contains(Filename, ".csv") == false && Contains(Filename, ".tsv") == false)
-    {
+	cout << "Please enter the name of the file containing the deck you would like to use." << endl;
+	cin >> Filename;
+	if(Contains(Filename, ".txt") == false && Contains(Filename, ".csv") == false && Contains(Filename, ".tsv") == false)
+	{
 		Filename = AddExtension(Filename);
 		if (Filename == "Q" || Filename == "q") { return 0; }
 	}
-	
-	// Check if the file can be opened
-	if(Contains(Filename, ".txt"))
-	{
-		while (newVillain.readTXTFile(Filename) == false)
+    
+    while(SuccessfulRead == false)
+    {
+		if(Contains(Filename, ".txt") == false && Contains(Filename, ".csv") == false && Contains(Filename, ".tsv") == false)
 		{
-			Filename = "";
-			cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
-			cin >> Filename;
+			Filename = AddExtension(Filename);
 			if (Filename == "Q" || Filename == "q") { return 0; }
-			if(Contains(Filename, ".txt") == false)
-			{ 
-				Filename = AddExtension(Filename);
+		}
+		
+		// Check if the file can be opened
+		if(Contains(Filename, ".txt"))
+		{
+			if(newVillain.readTXTFile(Filename) == false)
+			{
+				Filename = "";
+				cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
+				cin >> Filename;
 				if (Filename == "Q" || Filename == "q") { return 0; }
 			}
+			else {SuccessfulRead = true;}
 		}
-	}
-	else if(Contains(Filename, ".csv"))
-	{
-		//cout << "Attempting to read file." << endl;
-		while (newVillain.readCSVFile(Filename) == false)
+		else if(Contains(Filename, ".csv"))
 		{
-			Filename = "";
-			cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
-			cin >> Filename;
-			if (Filename == "Q" || Filename == "q") { return 0; }
-			if(Contains(Filename, ".csv") == false)
-			{ 
-				Filename = AddExtension(Filename);
+			//cout << "Attempting to read file." << endl;
+			if(newVillain.readCSVFile(Filename) == false)
+			{
+				Filename = "";
+				cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
+				cin >> Filename;
 				if (Filename == "Q" || Filename == "q") { return 0; }
 			}
+			else {SuccessfulRead = true;}
 		}
-	}
-	else if(Contains(Filename, ".tsv"))
-	{
-		//cout << "Attempting to read file." << endl;
-		while (newVillain.readTSVFile(Filename) == false)
+		else if(Contains(Filename, ".tsv"))
 		{
-			Filename = "";
-			cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
-			cin >> Filename;
-			if (Filename == "Q" || Filename == "q") { return 0; }
-			if(Contains(Filename, ".tsv") == false)
-			{ 
-				Filename = AddExtension(Filename);
+			//cout << "Attempting to read file." << endl;
+			if(newVillain.readTSVFile(Filename) == false)
+			{
+				Filename = "";
+				cout << "The file you have entered could not be opened, please enter a different filename or enter q to close the program." << endl;
+				cin >> Filename;
 				if (Filename == "Q" || Filename == "q") { return 0; }
 			}
+			else {SuccessfulRead = true;}
 		}
+		
 	}
 	
 	//newVillain.printDeck();
